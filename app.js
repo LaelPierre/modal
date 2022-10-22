c250cd5515b80f77fd488804399dd8127ade64a5
Lael Pierre <lael.aichaoui@gmail.com>
2022-10-21 20:15:37 -0700

    js open and close modal functionnality added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+// select items
+const btn = document.querySelector(".btn");
+const closeBtn = document.querySelector(".close-btn");
+const style = document.querySelector(".modal-overlay");
 
+// show modal functionnality
+btn.addEventListener("click", function () {
+  style.classList.add("show-modal");
+});
+
+// close modal functionnality
+closeBtn.addEventListener("click", function () {
+  style.classList.remove("show-modal");
+});
